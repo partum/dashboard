480682973c05ab56110777e170e1a2be24550440
partum <jarringt@uccs.edu>
2021-02-03 15:02:32 -0700

    finished add other tasks

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const time = document.getElementById('time'),
 greeting = document.getElementById('greeting'),
 name = document.getElementById('name'),
-focus = document.getElementById('focus');
+goal1 = document.getElementById('goal-1'),
+goal2 = document.getElementById('goal-2'),
+goal3 = document.getElementById('goal-3');
 
 //show time
 function showTime() {
@@ -70,35 +72,56 @@ function setName(e) {
 }
 
 //get focus
-// function getFocus() {
-//     if(localStorage.getItem('focus') === null){
-//         focus.textContent = '[Enter Focus]';
-//     }else{
-//         focus.textContent = localStorage.getItem('focus');
-//     }
-// }
+function getFocus() {
+    if(localStorage.getItem('goal-1') === null){
+        goal1.textContent = '[Enter Goal]';
+    }else{
+        goal1.textContent = localStorage.getItem('goal-1');
+    }
+    if(localStorage.getItem('goal-2') === null){
+        goal2.textContent = '[Enter Goal]';
+    }else{
+        goal2.textContent = localStorage.getItem('goal-2');
+    }
+    if(localStorage.getItem('goal-3') === null){
+        goal3.textContent = '[Enter Goal]';
+    }else{
+        goal3.textContent = localStorage.getItem('goal-3');
+    }
+}
 
 //set focus
-// function setFocus(e) {
-//     if(e.type === 'keypress') {
-//         //make sure enter is pressed
-//         if(e.which == 13 || e.keyCode == 13) {
-//             localStorage.setItem('focus', e.target.innerText);
-//             focus.blur();
-//         }
-//     }else{
-//         localStorage.setItem('focus', e.target.innerText);
-//     }
-// }
+function setFocus(e) {
+    if(e.type === 'keypress') {
+        //make sure enter is pressed
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem(this.id, e.target.innerText);
+            if(this.id=='goal-1'){
+                goal1.blur(); 
+            }
+            else if(this.id=='goal-2'){
+                goal2.blur(); 
+            }
+            else if(this.id=='goal-3'){
+                goal3.blur(); 
+            }   
+        }
+    }else{
+        localStorage.setItem(this.id, e.target.innerText);
+    }
+}
 
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
-//focus.addEventListener('keypress', setFocus);
-//focus.addEventListener('blur', setFocus);
+goal1.addEventListener('keypress', setFocus);
+goal1.addEventListener('blur', setFocus);
+goal2.addEventListener('keypress', setFocus);
+goal2.addEventListener('blur', setFocus);
+goal3.addEventListener('keypress', setFocus);
+goal3.addEventListener('blur', setFocus);
 
 //Run
 showTime();
 setBgGreet();
 getName();
-//getFocus();
-console.log("test");
+getFocus();
